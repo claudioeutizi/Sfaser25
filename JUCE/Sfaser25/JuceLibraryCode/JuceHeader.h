efd4ba29baa272b10fe262cbe4eaed998d50d9a8
mattiamassimi <mattia.massimi@mail.polimi.it>
2023-05-11 12:55:54 +0200

    Company name

--- JUCE/Sfaser25/JuceLibraryCode/JuceHeader.h
+++ JUCE/Sfaser25/JuceLibraryCode/JuceHeader.h
@@ -43,7 +43,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Sfaser25";
-    const char* const  companyName    = "";
+    const char* const  companyName    = "MCS";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
